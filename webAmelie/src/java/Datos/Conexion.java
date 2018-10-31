69e5ae3bd40a96ab5649c0b0e87c7d4225e9df1b
Felipe <Felipe@DESKTOP-0B0GQNU>
2018-10-31 09:45:52 -0300

    Add clases Cliente,Servlet RegistroUsuario, Cambios en Login.jsp y Conexion.java y Servlet VerificarLogin.

--- webAmelie/src/java/Datos/Conexion.java
+++ webAmelie/src/java/Datos/Conexion.java
@@ -5,6 +5,7 @@
  */
 package Datos;
 
+import Modelo.Cliente;
 import Modelo.Usuario;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -31,20 +32,47 @@ public class Conexion {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/aurora","root",null);
             state = conn.createStatement();
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e);
         }
     }
     
     public ArrayList<Usuario> listar() throws Exception{
         conexion();
         ArrayList<Usuario> users = new ArrayList<Usuario>();
-        ResultSet rs = state.executeQuery("SELECT * FROM usuarios");
+        ResultSet rs = state.executeQuery("SELECT * FROM usuario");
         while(rs.next()){
             Usuario user = new Usuario();
-            user.setUsuario((String) rs.getObject(1));
+            user.setnombreUsuario((String) rs.getObject(1));
             user.setPassword((String) rs.getObject(2));
             users.add(user);
         }
         return users;
+    }
+    
+    public boolean ingreso_datos(Cliente cliente, Usuario usuario){
+        boolean encontrado = false;
+        try {
+            conexion();
+            int i = state.executeUpdate("INSERT INTO cliente (Nombre,Apellido_Paterno,Apellido_Materno,email) VALUES ('"+cliente.getNombre()+"','"+cliente.getApellidoPaterno()+"','"+cliente.getApellidoMaterno()+"','"+cliente.getEmail()+"')");
+            
+            
+            
+            
+            int e = state.executeUpdate("INSERT INTO usuario (nombreUsuario,clave) VALUES ('"+usuario.getnombreUsuario()+"','"+usuario.getPassword()+"')");
+            
+            
+            
+            
+            
+            if(i > 0 & e > 0){
+                encontrado = true;
+            }
+            
+            
+        } catch (Exception e) {
+            e.getStackTrace();
+            System.out.println(e);
+        }
+        return encontrado;
     }
 }
---
+++ webAmelie/src/java/Modelo/Cliente.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Cliente {
+    
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String email;
+    
+    public Cliente(){
+        nombre="";
+        apellidoPaterno="";
+        apellidoMaterno="";
+        email="";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    
+    
+}
